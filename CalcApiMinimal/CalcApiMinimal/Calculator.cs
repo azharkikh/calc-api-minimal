3627cce2b51cbb4395c910f55684db99db8eaefa
Elizaveta Zotova <e.zotova@dodobrands.io>
2026-09-21 18:44:36 +0500

    add controllers

--- CalcApiMinimal/CalcApiMinimal/Calculator.cs
+++ CalcApiMinimal/CalcApiMinimal/Calculator.cs
@@ -6,4 +6,25 @@ public class Calculator
     {
         return a + b;
     }
+    
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Ошибка: Деление на ноль.");
+            return 1111;
+        }
+
+        return a / b;
+    }
+
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
 }
--- CalcApiMinimal/CalcApiMinimal/Program.cs
+++ CalcApiMinimal/CalcApiMinimal/Program.cs
@@ -14,6 +14,9 @@ public class Program
         // Configure the HTTP request pipeline.
 
         app.MapGet("/calculator/add", (double a, double b, Calculator calculator) => calculator.Add(a, b));
+        app.MapGet("/calculator/multiply", (double a, double b, Calculator calculator) => calculator.Multiply(a, b));
+        app.MapGet("/calculator/divide", (double a, double b, Calculator calculator) => calculator.Divide(a, b));
+        app.MapGet("/calculator/subtract", (double a, double b, Calculator calculator) => calculator.Subtract(a, b));
 
         app.Run();
     }
