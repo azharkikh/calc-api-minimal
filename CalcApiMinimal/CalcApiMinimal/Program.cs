68f7fe6075efec50e89fe045d475dc0437fea877
Elizaveta Zotova <e.zotova@dodobrands.io>
2026-09-24 10:07:53 +0500

    add swagger

--- CalcApiMinimal/CalcApiMinimal/Program.cs
+++ CalcApiMinimal/CalcApiMinimal/Program.cs
@@ -1,3 +1,5 @@
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 namespace CalcApiMinimal;
 
 public class Program
@@ -8,10 +10,21 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddSingleton<Calculator>();
+        builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new() { Title = "Calculator API", Version = "v1" });
+        }); 
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calculator API v1");
+        });
 
         app.MapGet("/calculator/add", (double a, double b, Calculator calculator) => calculator.Add(a, b));
         app.MapGet("/calculator/multiply", (double a, double b, Calculator calculator) => calculator.Multiply(a, b));
