0d460d3c4b72e46b9109d36e77770f0aafea8cf5
Elizaveta Zotova <e.zotova@dodobrands.io>
2026-09-25 17:29:57 +0500

    add unit tests

---
+++ CalcApiMinimal/CalcApi.UnitTests/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System.Diagnostics;
+using CalcApiMinimal;
+using NUnit.Framework;
+
+namespace CalcApi.UnitTests;
+
+[TestFixture]
+public class CalcTests
+{
+    public Calculator? Calculator { get; set; }
+    public ExtendedCalculator? ExtendedCalculator { get; set; }
+
+    [Test]
+    public void MultiplyTest()
+    {
+        //Arrange
+        Calculator = new Calculator();
+        double x = 2;
+        double y = 3;
+        
+        //Act
+        double z = Calculator.Multiply(x, y);
+
+        //Assert
+        Assert.That( x * y , Is.EqualTo(z));
+    }
+    
+    [Test]
+    public void AddTest()
+    {
+        //Arrange
+        Calculator = new Calculator();
+        double x = 2;
+        double y = 3;
+        
+        //Act
+        double z = Calculator.Add(x, y);
+
+        //Assert
+        Assert.That( x + y , Is.EqualTo(z));
+    }
+    
+    [Test]
+    public void DivideTest()
+    {
+        //Arrange
+        Calculator = new Calculator();
+        double x = 2;
+        double y = 3;
+        
+        //Act
+        double z = Calculator.Divide(x, y);
+
+        //Assert
+        Assert.That( x / y , Is.EqualTo(z));
+    }
+    
+    [Test]
+    public void SubtractTest()
+    {
+        //Arrange
+        Calculator = new Calculator();
+        double x = 2;
+        double y = 3;
+        
+        //Act
+        double z = Calculator.Subtract(x, y);
+
+        //Assert
+        Assert.That( x - y , Is.EqualTo(z));
+    }
+    
+    [Test]
+    public void CalculatePowerTest()
+    {
+        //Arrange
+        ExtendedCalculator = new ExtendedCalculator();
+        double x = 2;
+        double y = 3;
+        
+        //Act
+        double z = ExtendedCalculator.CalculatePower(x, y);
+
+        //Assert
+        Assert.That( Math.Pow(x , y) , Is.EqualTo(z));
+    }
+}
